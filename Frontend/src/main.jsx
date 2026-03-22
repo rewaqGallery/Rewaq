051a43261fca089c32f2afc6c3bbdef0929a01b4
mohamed <mohamedelafandy1@gamail.com>
2026-03-22 14:09:09 +0200

    frontend dir

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import App from "./App.jsx";
-import { store } from "./store/store";
+import { store } from "./store/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
